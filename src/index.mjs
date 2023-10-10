5a42a9f107d12228943c9c5b6a1951735279df34
oppata <147403631+oppata-a@users.noreply.github.com>
2023-10-10 14:20:27 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -80,10 +80,38 @@
  */
 
 //配列の展開
-const arr1 = [1, 2];
-// console.log(arr1);
-// console.log(...arr1);
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
 
-const sumFunc = (num1, num2) => console.log(num1 + num2);
-sumFunc(arr1[0], arr1[1]);
-sumFunc(...arr1);
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = [...arr4];
+// arr8[0] = 100;
+// console.log(`arr4:${arr4}`);
+// console.log(`arr8:${arr8}`);
+
+// //...で設定しないと同じ場所を参照してしまい値が同じになってしまう
+// const arr9 = arr4;
+// arr9[0] = 100;
+// console.log(`arr4:${arr4}`); //値が変わってしまう
+// console.log(`arr9:${arr9}`);
